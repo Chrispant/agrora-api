8ba5fe1efdad09e19efbd67745a07ed450e5299e
pantazopc <pantazopc@gmail.com>
2026-06-05 13:51:12 +0300

    cross origin for test website

--- src/main/java/gr/agrora_api/ContactMessageController.java
+++ src/main/java/gr/agrora_api/ContactMessageController.java
@@ -3,6 +3,8 @@ package gr.agrora_api;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+
+@CrossOrigin(origins = "https://elegant-visvesvaraya.213-158-90-234.plesk.page")
 @RestController
 @RequestMapping("api/contact")
 public class ContactMessageController {
